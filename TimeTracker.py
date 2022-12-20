2a73a680eacbb36c2f23f5f0539e796db14030a2
Felix Mansel <56174409+fansel@users.noreply.github.com>
2022-12-21 00:57:44 +0100

    Deleted Duration and fixed UI

--- TimeTracker.py
+++ TimeTracker.py
@@ -1,84 +1,97 @@
-
-import datetime
 import json
+import datetime
 
 class TimeTracker:
-    """Eine Klasse, die die Arbeitszeiten verwaltet und in einer JSON-Datei speichert."""
     def __init__(self):
         self.start_time = None
         self.end_time = None
         self.duration = None
-        self.timestamp = None
-    
+
+        
+
     def start(self):
-        """Erfasst die aktuelle Zeit als Startzeit."""
-        self.start_time = self.get_current_time()
-        self.timestamp = datetime.datetime.now()
+        """Starts the time tracker and records the current time as the start time."""
+        self.start_time = {"date": datetime.date.today().strftime("%d.%m.%Y"), "time": datetime.datetime.now().strftime("%H:%M:%S")}
 
     def stop(self):
-        """Erfasst die aktuelle Zeit als Endzeit und berechnet die Dauer in Minuten."""
-        self.end_time = self.get_current_time()
-        start = datetime.datetime.strptime(self.start_time["time"], "%H:%M:%S")
-        end = datetime.datetime.strptime(self.end_time["time"], "%H:%M:%S")
-        duration = end - start
-        self.duration = duration.total_seconds() / 60
-        
+        """Stops the time tracker and records the current time as the end time."""
+        self.end_time = {"date": datetime.date.today().strftime("%d.%m.%Y"), "time": datetime.datetime.now().strftime("%H:%M:%S")}
+        self.duration = self.get_duration()
+
+    def get_duration(self):
+        """Calculates the duration of the time tracker in minutes."""
+
+        date_start = datetime.datetime.strptime(self.start_time["date"], "%d.%m.%Y").date()
+        time_start = datetime.datetime.strptime(self.start_time["time"], "%H:%M:%S").time()
+        start_time = datetime.datetime.combine(date_start, time_start)
+        date_end = datetime.datetime.strptime(self.end_time["date"], "%d.%m.%Y").date()
+        time_end = datetime.datetime.strptime(self.end_time["time"], "%H:%M:%S").time()
+        end_time = datetime.datetime.combine(date_end, time_end)
+        duration = (end_time - start_time).total_seconds() / 60
+        rounded_duration = round(duration)
+        return rounded_duration
+
+
+
+
 
     def reset(self):
         """Setzt die Startzeit, Endzeit und Dauer zurück."""
         self.start_time = None
         self.end_time = None
-        self.duration = None
-        self.timestamp = None
 
     def save(self, filename,):
-        """Speichert die Startzeit, Endzeit und Dauer in der JSON-Datei."""
-        found = False
-        try:
-            with open(filename, "r") as read_file:
-                data = json.load(read_file)
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            data = {"time_entries": []}
-        for entry in data["time_entries"]:
-            if entry["start_time"] == self.start_time:
-                entry["end_time"] = self.end_time
-                entry["duration"] = self.duration
-                found = True
-                break
-        if not found:
-            data["time_entries"].append({
-                "start_time": self.start_time,
-                "end_time": self.end_time,
-                "duration": self.duration
-            })
-          
-        with open(filename, "w") as write_file:
-            json.dump(data, write_file, indent=4)
 
+            """Speichert die Startzeit, Endzeit und Dauer in der JSON-Datei."""
+            found = False
+            try:
+                with open(filename, "r") as read_file:
+                    data = json.load(read_file)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                data = {"time_entries": []}
+            for entry in data["time_entries"]:
+                if entry["start_time"] == self.start_time:
+                    entry["end_time"] = self.end_time
+                    found = True
+                    break
+            if not found:
+                data["time_entries"].append({
+                    "start_time": self.start_time,
+                    "end_time": self.end_time,
+                })
+            
+            with open(filename, "w") as write_file:
+                json.dump(data, write_file, indent=4)
 
     def load(self, filename):
-        """Lädt die Startzeit, Endzeit und Dauer aus der JSON-Datei."""
+        """Loads the start and end times of the time tracker from a JSON file."""
         with open(filename, "r") as f:
             data = json.load(f)
-        return data
+            return data
 
-    @staticmethod
-    def get_current_time():
-        """Erfasst die aktuelle Zeit und das aktuelle Datum und gibt sie als Dictionary zurück."""
-        current_time = datetime.datetime.now()
-        return {
-            "date": current_time.strftime("%d.%m.%Y"),
-            "time": current_time.strftime("%H:%M:%S")
-        }
-
-    def get_total_hours(self):
-        """Gibt die Summe der Dauer aller Einträge in der JSON-Datei zurück."""
-        total_hours = 0
-        with open("times.json", "r") as f:
+    def get_total_hours(self, filename):
+        """Gibt die Gesamtarbeitszeit für das Projekt zurück."""
+        with open(filename, "r") as f:
             data = json.load(f)
-        for entry in data["time_entries"]:
-            if entry["duration"] is not None:
-                total_hours += entry["duration"]
-        total_hours = round(total_hours /60, 2)
-        return total_hours 
+            total = 0
+            for entry in data["time_entries"]:
+                if entry["start_time"] == None:
+                    continue
+                elif entry["start_time"] == "Null":
+                    continue
+                elif entry["end_time"] == "Null":
+                    continue
+                elif entry["end_time"] == None:
+                    continue
+
+                date_start = datetime.datetime.strptime(entry["start_time"]["date"], "%d.%m.%Y").date()
+                time_start = datetime.datetime.strptime(entry["start_time"]["time"], "%H:%M:%S").time()
+                start_time = datetime.datetime.combine(date_start, time_start)
+                date_end = datetime.datetime.strptime(entry["end_time"]["date"], "%d.%m.%Y").date()
+                time_end = datetime.datetime.strptime(entry["end_time"]["time"], "%H:%M:%S").time()
+                end_time = datetime.datetime.combine(date_end, time_end)
+                duration = (end_time - start_time).total_seconds() / 3600
+                total += duration
+            return round(total,2)
+
 
--- TimeTrackerGUI.py
+++ TimeTrackerGUI.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-from TimeTracker import TimeTracker
-from TimeTracker import TimeTracker
+from TimeTracker import TimeTracker 
 import datetime
 
 
@@ -15,30 +14,42 @@ class TimeTrackerGUI:
         self.timetable.pack()
         self.clear_button = tk.Button(master, text="Clear", command=self.on_clear_button_click)
         self.clear_button.pack()
-        self.hours = tk.Label(master, text="Gesamtstunden: "+str(self.tracker.get_total_hours()))
-        self.hours.pack()
+        self.hours = tk.Label(master, text="Gesamtstunden: "+str(self.tracker.get_total_hours("times.json")))
+        #put the label to the right bottom corner
+        self.hours.place(relx=1.0, rely=1.0, anchor='se')
         self.export_button = tk.Button(master, text="Exportieren", command=self.on_export_button_click)
-        self.export_button.pack()
+        self.export_button.place(relx=1.0, rely=0.0, anchor='ne')
         self.timetable.heading("#0", text="Datum")
         self.timetable.heading("#1", text="Startzeit")
         self.timetable.heading("#2", text="Endzeit")
         self.timetable.heading("#3", text="Dauer in Minuten")
-        self.filter = datetime.datetime.now()
-        self.dropdown = tk.OptionMenu(master, self.filter, "Heute", "Letzte Woche", "Letzter Monat", "Letztes Jahr", "Alle")
-        self.dropdown.pack()
+        self.swichDropdown = tk.StringVar(master)
+        self.swichDropdown.set("Seit Programmstart")
+        self.progstart = datetime.datetime.now()
+        self.filter = self.progstart
+        self.update_table()
+        self.dropdown = tk.OptionMenu(master, self.swichDropdown, "Seit Programmstart","Heute", "Letzte Woche", "Letzter Monat", "Letztes Jahr", "Alle", command=self.dropdown)  # Add command argument
+        #place the dropdown menu to the left of the export button
+        self.dropdown.place(relx=0.0, rely=0.0, anchor='nw')
+        master.resizable(False, False)
+        master.title("TimeTracker")
 
-    def dropdown(self):
-        """Wird aufgerufen, wenn ein neuer Filter ausgewählt wird."""
-        if self.filter == "Heute":
-            self.filter = datetime.date()
-        elif self.filter == "Letzte Woche":
-            self.filter = datetime.date() - datetime.timedelta(days=7)
-        elif self.filter == "Letzter Monat":
-            self.filter = datetime.date() - datetime.timedelta(days=30)
-        elif self.filter == "Letztes Jahr":
-            self.filter = datetime.date() - datetime.timedelta(days=365)
-        elif self.filter == "Alle":
-            self.filter = 0
+
+    def dropdown(self, value):
+        """Updates the filter attribute based on the selection in the dropdown menu."""
+        if value == "Seit Programmstart":
+            self.filter = self.progstart
+        elif value == "Heute":
+            self.filter = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        elif value == "Letzte Woche":
+            self.filter = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=7)
+        elif value == "Letzter Monat":
+            self.filter = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=30)
+        elif value == "Letztes Jahr":
+            self.filter = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=365)
+        elif value == "Alle":
+            self.filter = datetime.datetime(1970, 1, 1, 0, 0, 0, 0)
+        self.update_table()
 
 
 
@@ -64,7 +75,6 @@ class TimeTrackerGUI:
             self.timetable.insert("", "end", text=self.tracker.start_time["date"], values=(self.tracker.start_time["time"], "", ""))
             self.play_button.config(text="Stoppen")
             self.play_button.config(command=self.on_pause_button_click)
-            self.update()
 
 
 
@@ -93,22 +103,22 @@ class TimeTrackerGUI:
         self.timetable.delete(*self.timetable.get_children())
         # Lade die Daten aus der JSON-Datei
         data = self.tracker.load("times.json")
-        # Setze Standardwerte für Start- und Endzeit sowie Dauer
-        start_time = {"date": "", "time": ""}
-        end_time = {"date": "", "time": ""}
-        duration = ""
-        # Überschreibe Standardwerte mit Werten aus der JSON-Datei, wenn vorhanden
-        if "start_time" in data:
-            start_time = data["start_time"]
-        if "end_time" in data:
-            end_time = data["end_time"]
-        if "duration" in data:
-            duration = data["duration"]
         for item in data["time_entries"]:
+            if item["start_time"] is not None:
+                start_time = item["start_time"]
+            else:
+                start_time = {"date": "", "time": ""}
+            if item["end_time"] is not None:
+                end_time = item["end_time"]
+            else:
+                end_time = {"date": "", "time": ""}
             if item is not None:
-                if item["start_time"]["date"] >= self.filter.strftime("%d.%m.%Y") and item["start_time"]["time"] >= self.filter.strftime("%H:%M:%S"):
-                  self.timetable.insert("", "end", text=item["start_time"]["date"], values=(item["start_time"]["time"], item["end_time"]["time"], round(item["duration"], 1)))
-        self.hours.config(text="Gesamtstunden: "+str(self.tracker.get_total_hours()))
+                date_start = datetime.datetime.strptime(item["start_time"]["date"], "%d.%m.%Y").date()
+                time_start = datetime.datetime.strptime(item["start_time"]["time"], "%H:%M:%S").time()
+                start_time = datetime.datetime.combine(date_start, time_start)
+                if start_time >= self.filter:
+                  self.timetable.insert("", "end", text=item["start_time"]["date"], values=(item["start_time"]["time"], item["end_time"]["time"],self.tracker.duration))
+        self.hours.config(text="Gesamtstunden: "+str(self.tracker.get_total_hours("times.json"))+" Stunden")
 
 
 
